7a9af411e05c5ae58613539fdeb9658ecac31856
Fernando Espinosa <fernando@polymath.network>
2019-04-01 23:51:29 -0400

    chore: yarn add --dev tsconfig-paths-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,19 @@
+const { TsconfigPathsPlugin } = require('tsconfig-paths-webpack-plugin');
+
 module.exports = {
     entry: './src/index.ts',
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+        plugins: [
+            new TsconfigPathsPlugin()
+        ]
+    },
     module: {
         rules: [
             {
                 test: /\.tsx?$/,
                 exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader"
-                }
+                loader: "babel-loader"
             }
         ]
     }
