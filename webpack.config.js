3095532a0754a20cd0fb2bb0ac1607e19fd9c59d
Fernando Espinosa <fernando@polymath.network>
2019-04-01 23:16:46 -0400

    chore: yarn add --dev babel-loader @babel/preset-env

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    entry: './src/index.ts',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            }
+        ]
+    }
+};
