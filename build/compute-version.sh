dbb3be3ceb88855e54bb5d18d6dea809860144bd
Fernando Espinosa <fernando@polymath.network>
2019-04-22 17:42:54 -0400

    fix: build/compute-version.sh

--- build/compute-version.sh
+++ build/compute-version.sh
@@ -3,6 +3,6 @@
 TAG_VERSION=`git describe --abbrev=0 $BUILD_VCS_NUMBER --tags`
 GIT_HASH_SHORT=${BUILD_VCS_NUMBER:0:7}
 
-echo "##teamcity[buildNumber '$TAG_VERSION-$GIT_HASH_SHORT.$BUILD_NUMBER']"
+echo "##teamcity[buildNumber '$GIT_HASH_SHORT.$BUILD_NUMBER ($TAG_VERSION)']"
 
 exit 0
