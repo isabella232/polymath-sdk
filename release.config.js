7229f4c2a6c687ed804f375c041f7086606bdf90
Fernando Espinosa <fernando@polymath.network>
2019-04-22 14:22:59 -0400

    perf: `@semantic-release/npm` step

--- release.config.js
+++ release.config.js
@@ -15,9 +15,14 @@ module.exports = {
     },
   ],
   /**
-   * In this order the
+   * In this order the **prepare** step of @semantic-release/npm will run first
+   * followed by @semantic-release/git:
+   *  - Update the package.json version and create the npm package tarball
+   *  - Push a release commit and tag, including configurable files
    *
-   *
+   * See:
+   *  - https://github.com/semantic-release/semantic-release/blob/beta/docs/usage/plugins.md#plugin-ordering
+   *  - https://github.com/semantic-release/semantic-release/blob/beta/docs/extending/plugins-list.md
    */
   plugins: [
     '@semantic-release/commit-analyzer',
