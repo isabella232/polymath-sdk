3f084bd96894c5bdd1e0900798d4ea8514d98ef3
Jeremias Diaz <monitz87@gmail.com>
2019-04-24 16:23:18 -0400

    feat: make uuids unserializable

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,31 +1,6 @@
 import { isAddress } from '~/LowLevel/utils';
-import { Pojo, isPojo } from '~/types';
-import { join, keys } from 'lodash';
-
-/**
- * Returns a string hash of a POJO for comparison
- *
- * @param args arguments to hash
- */
-function hashObj(args: any): string {
-  const sortedKeyArray = keys(args).sort();
-
-  return join(
-    sortedKeyArray.map(key => {
-      const val = args[key];
-      let result;
-
-      if (isPojo(val)) {
-        result = hashObj(val);
-      } else {
-        result = `${args[key]}`;
-      }
-
-      return `${key}:${result}`;
-    }),
-    ','
-  );
-}
+import { Pojo } from '~/types';
+import stringify from 'json-stable-stringify';
 
 export const delay = async (amount: number) => {
   return new Promise(resolve => {
@@ -36,8 +11,21 @@ export const delay = async (amount: number) => {
 };
 
 export function serialize(entityType: string, pojo: Pojo) {
-  const hashed = hashObj(pojo);
-  return `${entityType}:${Buffer.from(hashed).toString('base64')}`;
+  return `${entityType}:${Buffer.from(stringify(pojo)).toString('base64')}`;
+}
+
+export function unserialize(id: string) {
+  const matched = id.match(/^.*:(.*)/);
+
+  if (!matched) {
+    throw new Error('Wrong ID format.');
+  }
+
+  const [, serialized] = matched;
+
+  const jsonString = Buffer.from(serialized, 'base64').toString('utf8');
+
+  return JSON.parse(jsonString);
 }
 
 export function isValidAddress(address: string) {
