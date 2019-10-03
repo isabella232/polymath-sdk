bb9a67810cb2f78ae617be58d8d2205e7f9f39ed
Victor Wiebe <victor.wiebe@polymath.network>
2019-10-03 13:15:27 -0800

    feat: try using typemoq instead of ts-mockito for same test

--- src/entities/__tests__/Shareholders.ts
+++ src/entities/__tests__/Shareholders.ts
@@ -1,18 +1,13 @@
 import { Context } from '~/Context';
-import { instance, mock, when } from 'ts-mockito';
 import { SecurityToken } from '../SecurityToken';
 import { Shareholders } from '~/entities/SecurityToken/Shareholders';
 import { SubModule } from '~/entities/SecurityToken/SubModule';
-
 import { CreateCheckpoint } from '../../procedures/CreateCheckpoint';
-
-// This mock must be prefixed with "mock" to work, i.e. mockName
-const mockPrepare = jest.fn();
-jest.mock('../../procedures/CreateCheckpoint', () => ({
-  CreateCheckpoint: jest.fn(() => ({
-    prepare: mockPrepare,
-  })),
-}));
+import { PolymathBase } from '~/PolymathBase';
+import * as TypeMoq from 'typemoq';
+import { IMock } from 'typemoq';
+import { IGlobalMock } from 'typemoq/Api/IGlobalMock';
+import { Web3ProviderEngine } from '@0x/subproviders';
 
 const params1 = {
   symbol: 'TEST1',
@@ -23,14 +18,39 @@ const params1 = {
 
 describe('Shareholders', () => {
   let target: Shareholders;
+  let mockSecurityToken: IMock<SecurityToken>;
+  let mockCreateCheckpoint: IGlobalMock<CreateCheckpoint>;
 
   beforeAll(() => {
-    const contextMock = mock(Context);
-    const SecurityTokenMock = mock(SecurityToken);
-    when(SecurityTokenMock.uid).thenReturn('12345');
-    when(SecurityTokenMock.symbol).thenReturn(params1.symbol);
-    const stMock = instance(SecurityTokenMock);
-    target = new Shareholders(stMock, contextMock);
+    // This mock must be prefixed with "mock" to work, i.e. mockName
+    /*
+    const mockPrepare = jest.fn();
+    jest.mock('../../procedures/CreateCheckpoint', () => ({
+      CreateCheckpoint: jest.fn(() => ({
+        prepare: mockPrepare,
+      })),
+    }));
+    */
+    mockCreateCheckpoint = TypeMoq.GlobalMock.ofType2<CreateCheckpoint>('CreateCheckpoint', global);
+
+    const provider = new Web3ProviderEngine();
+    provider.start();
+    const mockProvider: TypeMoq.IMock<Web3ProviderEngine> = TypeMoq.Mock.ofInstance(provider);
+    const polymathBase = new PolymathBase({ provider: mockProvider.object });
+    const mockPolymathBase: TypeMoq.IMock<PolymathBase> = TypeMoq.Mock.ofInstance(polymathBase);
+    const context = new Context({ contractWrappers: mockPolymathBase.object });
+    const mockContext: TypeMoq.IMock<Context> = TypeMoq.Mock.ofInstance(context);
+    const st1 = new SecurityToken(params1, mockContext.object);
+
+    // const SecurityTokenMock = mock(SecurityToken);
+    mockSecurityToken = TypeMoq.Mock.ofInstance(st1);
+    // when(SecurityTokenMock.uid).thenReturn('12345');
+    mockSecurityToken.setup(x => x.uid).returns((key: string) => '12345');
+    // when(SecurityTokenMock.symbol).thenReturn(params1.symbol);
+    mockSecurityToken.setup(x => x.uid).returns((key: string) => params1.symbol);
+
+    // Instantiate Shareholders
+    target = new Shareholders(mockSecurityToken.object, mockContext.object);
   });
 
   describe('Types', () => {
@@ -46,8 +66,9 @@ describe('Shareholders', () => {
 
       // Verifications
       spyOn(target, 'createCheckpoint').wasCalled;
-      expect(CreateCheckpoint).toHaveBeenCalledTimes(1);
-      expect(mockPrepare).toHaveBeenCalledTimes(1);
+      TypeMoq.GlobalScope.using(mockCreateCheckpoint).with(() => {
+        expect(mockCreateCheckpoint.object.prepare()).toHaveBeenCalledTimes(1);
+      });
     });
   });
 
