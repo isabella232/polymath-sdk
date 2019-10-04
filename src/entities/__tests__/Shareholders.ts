f3b3333a053a0edd1297b46508f8c23d28a53bfd
Victor Wiebe <victor.wiebe@polymath.network>
2019-10-04 09:25:06 -0800

    feat: use ts-mock-imports and sinon to test CreateCheckpoint

--- src/entities/__tests__/Shareholders.ts
+++ src/entities/__tests__/Shareholders.ts
@@ -1,13 +1,11 @@
-import { Context } from '~/Context';
-import { SecurityToken } from '../SecurityToken';
+import * as sinon from 'sinon';
+import { ImportMock, MockManager } from 'ts-mock-imports';
+import * as contextObject from '../../Context';
+import * as createCheckpointProcedure from '../../procedures/CreateCheckpoint';
+
 import { Shareholders } from '~/entities/SecurityToken/Shareholders';
 import { SubModule } from '~/entities/SecurityToken/SubModule';
-import { CreateCheckpoint } from '../../procedures/CreateCheckpoint';
-import { PolymathBase } from '~/PolymathBase';
-import * as TypeMoq from 'typemoq';
-import { IMock } from 'typemoq';
-import { IGlobalMock } from 'typemoq/Api/IGlobalMock';
-import { Web3ProviderEngine } from '@0x/subproviders';
+import { SecurityToken } from '../SecurityToken';
 
 const params1 = {
   symbol: 'TEST1',
@@ -18,39 +16,18 @@ const params1 = {
 
 describe('Shareholders', () => {
   let target: Shareholders;
-  let mockSecurityToken: IMock<SecurityToken>;
-  let mockCreateCheckpoint: IGlobalMock<CreateCheckpoint>;
+  let createCheckpointMock: MockManager<createCheckpointProcedure.CreateCheckpoint>;
 
   beforeAll(() => {
-    // This mock must be prefixed with "mock" to work, i.e. mockName
-    /*
-    const mockPrepare = jest.fn();
-    jest.mock('../../procedures/CreateCheckpoint', () => ({
-      CreateCheckpoint: jest.fn(() => ({
-        prepare: mockPrepare,
-      })),
-    }));
-    */
-    mockCreateCheckpoint = TypeMoq.GlobalMock.ofType2<CreateCheckpoint>('CreateCheckpoint', global);
+    // Generate the mock for CreateCheckpoint
+    createCheckpointMock = ImportMock.mockClass(createCheckpointProcedure, 'CreateCheckpoint');
 
-    const provider = new Web3ProviderEngine();
-    provider.start();
-    const mockProvider: TypeMoq.IMock<Web3ProviderEngine> = TypeMoq.Mock.ofInstance(provider);
-    const polymathBase = new PolymathBase({ provider: mockProvider.object });
-    const mockPolymathBase: TypeMoq.IMock<PolymathBase> = TypeMoq.Mock.ofInstance(polymathBase);
-    const context = new Context({ contractWrappers: mockPolymathBase.object });
-    const mockContext: TypeMoq.IMock<Context> = TypeMoq.Mock.ofInstance(context);
-    const st1 = new SecurityToken(params1, mockContext.object);
-
-    // const SecurityTokenMock = mock(SecurityToken);
-    mockSecurityToken = TypeMoq.Mock.ofInstance(st1);
-    // when(SecurityTokenMock.uid).thenReturn('12345');
-    mockSecurityToken.setup(x => x.uid).returns((key: string) => '12345');
-    // when(SecurityTokenMock.symbol).thenReturn(params1.symbol);
-    mockSecurityToken.setup(x => x.uid).returns((key: string) => params1.symbol);
+    // Generate a mock for context, and a security token to instantiate Shareholders
+    const contextMock = ImportMock.mockClass(contextObject, 'Context');
+    const st1 = new SecurityToken(params1, contextMock.getMockInstance());
 
     // Instantiate Shareholders
-    target = new Shareholders(mockSecurityToken.object, mockContext.object);
+    target = new Shareholders(st1, contextMock.getMockInstance());
   });
 
   describe('Types', () => {
@@ -61,20 +38,21 @@ describe('Shareholders', () => {
 
   describe('createCheckpoint', () => {
     test('should send the transaction to createCheckpoint', async () => {
+      // Setup the mock on prepare
+      const prepareCreateCheckpointMock = createCheckpointMock.mock('prepare', {});
+
       // Real call
       await target.createCheckpoint();
 
       // Verifications
-      spyOn(target, 'createCheckpoint').wasCalled;
-      TypeMoq.GlobalScope.using(mockCreateCheckpoint).with(() => {
-        expect(mockCreateCheckpoint.object.prepare()).toHaveBeenCalledTimes(1);
-      });
+      expect(sinon.spy(target, 'createCheckpoint').calledOnce);
+      expect(prepareCreateCheckpointMock.calledOnce);
     });
   });
 
   /*
   // Original test we created here, saving it as a comment as it is useful to test
-  // use of wrappers in procedures afterwards.
+  // use of wrappers in procedures afterwards. Completed with ts-mockito
   describe('createCheckpoint', () => {
     beforeAll(() => {
       MockPolymathBase = mock(PolymathBase);
