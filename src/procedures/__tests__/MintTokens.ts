403836c7413c4579cd786689a2c6b8704b5a6a8f
Victor Wiebe <victor.wiebe@polymath.network>
2019-10-23 16:28:26 -0800

    feat: skeleton for mint tokens, issues persist with generateId

---
+++ src/procedures/__tests__/MintTokens.ts
@@ -0,0 +1,286 @@
+import * as sinon from 'sinon';
+import { ImportMock, MockManager } from 'ts-mock-imports';
+import { SinonStub } from 'sinon';
+import BigNumber from 'bignumber.js';
+import { TransactionReceiptWithDecodedLogs } from 'ethereum-protocol';
+import * as contractWrappersModule from '@polymathnetwork/contract-wrappers';
+import { MintTokens } from '../../procedures/MintTokens';
+import { Procedure } from '~/procedures/Procedure';
+import * as shareholdersEntityModule from '~/entities/SecurityToken/Shareholders';
+import * as securityTokenEntityModule from '~/entities/SecurityToken/SecurityToken';
+import { PolymathError } from '~/PolymathError';
+import { ErrorCode, MintTokensProcedureArgs, ProcedureType } from '~/types';
+import * as securityTokenFactoryModule from '~/entities/factories/SecurityTokenFactory';
+import * as cappedStoFactoryModule from '~/entities/factories/CappedStoFactory';
+import * as checkpointFactoryModule from '~/entities/factories/CheckpointFactory';
+import * as dividendDistributionSecurityTokenFactoryModule from '~/entities/factories/DividendDistributionFactory';
+import * as erc20DividendsManagerFactoryModule from '~/entities/factories/Erc20DividendsManagerFactory';
+import * as erc20TokenBalanceFactoryModule from '~/entities/factories/Erc20TokenBalanceFactory';
+import * as ethDividendsManagerFactoryModule from '~/entities/factories/EthDividendsManagerFactory';
+import * as investmentFactoryModule from '~/entities/factories/InvestmentFactory';
+import * as securityTokenReservationModule from '~/entities/factories/SecurityTokenReservationFactory';
+import * as shareholderFactoryModule from '~/entities/factories/ShareholderFactory';
+import * as usdTieredStoFactoryModule from '~/entities/factories/UsdTieredStoFactory';
+import * as taxWithholdingFactoryModule from '~/entities/factories/TaxWithholdingFactory';
+import * as utilsModule from '~/utils';
+import * as contextModule from '../../Context';
+import * as wrappersModule from '../../PolymathBase';
+import * as tokenFactoryModule from '../../testUtils/MockedTokenFactoryModule';
+import * as moduleWrapperFactoryModule from '../../testUtils/MockedModuleWrapperFactoryModule';
+import { Wallet } from '~/Wallet';
+
+const testAddress = '0x6666666666666666666666666666666666666666';
+const testAddress2 = '0x9999999999999999999999999999999999999999';
+const params1: MintTokensProcedureArgs = {
+  symbol: 'TEST1',
+  mintingData: [
+    {
+      address: testAddress,
+      amount: new BigNumber(1),
+    },
+  ],
+};
+
+describe('MintTokens', () => {
+  let target: MintTokens;
+  let contextMock: MockManager<contextModule.Context>;
+  let wrappersMock: MockManager<wrappersModule.PolymathBase>;
+  let tokenFactoryMock: MockManager<tokenFactoryModule.MockedTokenFactoryModule>;
+  let moduleWrapperFactoryMock: MockManager<
+    moduleWrapperFactoryModule.MockedModuleWrapperFactoryModule
+  >;
+  let polyTokenMock: MockManager<contractWrappersModule.PolyToken>;
+  let tokenFactoryStub: SinonStub<any, any>;
+  let moduleWrapperFactoryStub: SinonStub<any, any>;
+
+  // Mock factories
+  let securityTokenFactoryMock: MockManager<securityTokenFactoryModule.SecurityTokenFactory>;
+  let cappedStoFactoryMock: MockManager<cappedStoFactoryModule.CappedStoFactory>;
+  let checkpointFactoryMock: MockManager<checkpointFactoryModule.CheckpointFactory>;
+  let dividendDistributionFactoryMock: MockManager<
+    dividendDistributionSecurityTokenFactoryModule.DividendDistributionFactory
+  >;
+  let erc20DividendsManagerFactoryMock: MockManager<
+    erc20DividendsManagerFactoryModule.Erc20DividendsManagerFactory
+  >;
+  let erc20TokenBalanceFactoryMock: MockManager<
+    erc20TokenBalanceFactoryModule.Erc20TokenBalanceFactory
+  >;
+  let ethDividendsManagerFactoryMock: MockManager<
+    ethDividendsManagerFactoryModule.EthDividendsManagerFactory
+  >;
+  let investmentFactoryMock: MockManager<investmentFactoryModule.InvestmentFactory>;
+  let securityTokenReservationFactoryMock: MockManager<
+    securityTokenReservationModule.SecurityTokenReservationFactory
+  >;
+  let shareholderFactoryMock: MockManager<shareholderFactoryModule.ShareholderFactory>;
+  let usdTieredStoFactoryMock: MockManager<usdTieredStoFactoryModule.UsdTieredStoFactory>;
+  let taxWithholdingFactoryMock: MockManager<taxWithholdingFactoryModule.TaxWithholdingFactory>;
+  let securityTokenMock: MockManager<contractWrappersModule.SecurityToken_3_0_0>;
+  let moduleFactoryMock: MockManager<contractWrappersModule.ModuleFactory_3_0_0>;
+
+  let shareholdersEntityMock: MockManager<shareholdersEntityModule.Shareholders>;
+
+  let securityTokenEntityMock: MockManager<securityTokenEntityModule.SecurityToken>;
+
+  let findEventsStub: SinonStub<any, any>;
+  let getAttachedModulesFactoryAddressStub: SinonStub<any, any>;
+
+  beforeAll(() => {
+    // Mock the context, wrappers, and tokenFactory to test MintTokens
+    contextMock = ImportMock.mockClass(contextModule, 'Context');
+    wrappersMock = ImportMock.mockClass(wrappersModule, 'PolymathBase');
+    tokenFactoryMock = ImportMock.mockClass(tokenFactoryModule, 'MockedTokenFactoryModule');
+    moduleWrapperFactoryMock = ImportMock.mockClass(
+      moduleWrapperFactoryModule,
+      'MockedModuleWrapperFactoryModule'
+    );
+
+    shareholdersEntityMock = ImportMock.mockClass(shareholdersEntityModule, 'Shareholders');
+
+    securityTokenEntityMock = ImportMock.mockClass(securityTokenEntityModule, 'SecurityToken');
+
+    contextMock.set('contractWrappers', wrappersMock.getMockInstance());
+    wrappersMock.set('tokenFactory', tokenFactoryMock.getMockInstance());
+    wrappersMock.set('moduleFactory', moduleWrapperFactoryMock.getMockInstance());
+
+    securityTokenMock = ImportMock.mockClass(contractWrappersModule, 'SecurityToken_3_0_0');
+    securityTokenMock.mock('address', Promise.resolve(testAddress));
+    securityTokenMock.mock('balanceOf', Promise.resolve(new BigNumber(1)));
+
+    moduleFactoryMock = ImportMock.mockClass(contractWrappersModule, 'ModuleFactory_3_0_0');
+    moduleFactoryMock.mock('setupCostInPoly', Promise.resolve(new BigNumber(1)));
+    moduleFactoryMock.mock('isCostInPoly', Promise.resolve(false));
+    moduleFactoryMock.mock('setupCost', Promise.resolve(new BigNumber(1)));
+
+    tokenFactoryStub = tokenFactoryMock.mock(
+      'getSecurityTokenInstanceFromTicker',
+      securityTokenMock.getMockInstance()
+    );
+    moduleWrapperFactoryStub = moduleWrapperFactoryMock.mock(
+      'getModuleFactory',
+      moduleFactoryMock.getMockInstance()
+    );
+
+    securityTokenFactoryMock = ImportMock.mockClass(
+      securityTokenFactoryModule,
+      'SecurityTokenFactory'
+    );
+
+    cappedStoFactoryMock = ImportMock.mockClass(cappedStoFactoryModule, 'CappedStoFactory');
+    checkpointFactoryMock = ImportMock.mockClass(checkpointFactoryModule, 'CheckpointFactory');
+    dividendDistributionFactoryMock = ImportMock.mockClass(
+      dividendDistributionSecurityTokenFactoryModule,
+      'DividendDistributionFactory'
+    );
+    erc20DividendsManagerFactoryMock = ImportMock.mockClass(
+      erc20DividendsManagerFactoryModule,
+      'Erc20DividendsManagerFactory'
+    );
+    erc20TokenBalanceFactoryMock = ImportMock.mockClass(
+      erc20TokenBalanceFactoryModule,
+      'Erc20TokenBalanceFactory'
+    );
+    ethDividendsManagerFactoryMock = ImportMock.mockClass(
+      ethDividendsManagerFactoryModule,
+      'EthDividendsManagerFactory'
+    );
+    investmentFactoryMock = ImportMock.mockClass(investmentFactoryModule, 'InvestmentFactory');
+    securityTokenReservationFactoryMock = ImportMock.mockClass(
+      securityTokenReservationModule,
+      'SecurityTokenReservationFactory'
+    );
+    shareholderFactoryMock = ImportMock.mockClass(shareholderFactoryModule, 'ShareholderFactory');
+    usdTieredStoFactoryMock = ImportMock.mockClass(
+      usdTieredStoFactoryModule,
+      'UsdTieredStoFactory'
+    );
+    taxWithholdingFactoryMock = ImportMock.mockClass(
+      taxWithholdingFactoryModule,
+      'TaxWithholdingFactory'
+    );
+
+    const factoryMockSetup = {
+      securityTokenFactory: securityTokenFactoryMock.getMockInstance(),
+      securityTokenReservationFactory: securityTokenReservationFactoryMock.getMockInstance(),
+      erc20TokenBalanceFactory: erc20TokenBalanceFactoryMock.getMockInstance(),
+      investmentFactory: investmentFactoryMock.getMockInstance(),
+      cappedStoFactory: cappedStoFactoryMock.getMockInstance(),
+      usdTieredStoFactory: usdTieredStoFactoryMock.getMockInstance(),
+      dividendDistributionFactory: dividendDistributionFactoryMock.getMockInstance(),
+      checkpointFactory: checkpointFactoryMock.getMockInstance(),
+      erc20DividendsManagerFactory: erc20DividendsManagerFactoryMock.getMockInstance(),
+      ethDividendsManagerFactory: ethDividendsManagerFactoryMock.getMockInstance(),
+      shareholderFactory: shareholderFactoryMock.getMockInstance(),
+      taxWithholdingFactory: taxWithholdingFactoryMock.getMockInstance(),
+    };
+    contextMock.set('factories', factoryMockSetup);
+    contextMock.set('currentWallet', new Wallet({ address: () => Promise.resolve(testAddress) }));
+
+    polyTokenMock = ImportMock.mockClass(contractWrappersModule, 'PolyToken');
+    polyTokenMock.mock('balanceOf', Promise.resolve(new BigNumber(2)));
+    polyTokenMock.mock('address', Promise.resolve(testAddress));
+    polyTokenMock.mock('allowance', Promise.resolve(new BigNumber(0)));
+    wrappersMock.set('polyToken', polyTokenMock.getMockInstance());
+    wrappersMock.mock('isTestnet', Promise.resolve(false));
+
+    getAttachedModulesFactoryAddressStub = wrappersMock.mock(
+      'getModuleFactoryAddress',
+      Promise.resolve(testAddress)
+    );
+
+    const shareHolders = [
+      {
+        address: testAddress,
+        canSendAfter: new Date(Date.now()),
+        canReceiveAfter: new Date(Date.now()),
+        kycExpiry: new Date(Date.now()),
+        canBuyFromSto: true,
+        isAccredited: true,
+      },
+      {
+        address: testAddress2,
+        canSendAfter: new Date(Date.now()),
+        canReceiveAfter: new Date(Date.now()),
+        kycExpiry: new Date(Date.now()),
+        canBuyFromSto: true,
+        isAccredited: true,
+      },
+    ];
+
+    shareholdersEntityMock.mock('getShareholders', shareHolders);
+    securityTokenEntityMock.mock('shareholders', shareholdersEntityMock.getMockInstance());
+
+    securityTokenFactoryMock.mock('fetch', securityTokenEntityMock.getMockInstance());
+
+    // Instantiate MintTokens
+    target = new MintTokens(params1, contextMock.getMockInstance());
+  });
+  afterEach(() => {
+    sinon.restore();
+  });
+
+  describe('Types', () => {
+    test('should extend procedure and have MintTokens type', async () => {
+      expect(target instanceof Procedure).toBe(true);
+      expect(target.type).toBe(ProcedureType.MintTokens);
+    });
+  });
+
+  describe('MintTokens', () => {
+    test('should send the transaction to MintTokens', async () => {
+      const spyOnAddProcedure = sinon.spy(target, 'addProcedure');
+      const spyOnAddTransaction = sinon.spy(target, 'addTransaction');
+      // Real call
+      await target.prepareTransactions();
+
+      // Verifications
+      expect(
+        spyOnAddTransaction.withArgs(securityTokenMock.getMockInstance().addModuleWithLabel)
+          .callCount
+      ).toBe(1);
+      // TODO add with correct args
+      expect(spyOnAddProcedure.callCount).toBe(1);
+    });
+
+    test('should correctly return the resolver', async () => {
+      const shareholderObject = {
+        shareholder: {
+          securityTokenId: () => Promise.resolve(params1.symbol),
+          address: () => Promise.resolve(testAddress),
+        },
+      };
+      const fetchStub = shareholderFactoryMock.mock('fetch', shareholderObject);
+      findEventsStub = ImportMock.mockFunction(utilsModule, 'findEvents', [
+        {
+          args: {
+            _module: '0x3333333333333333333333333333333333333333',
+          },
+        },
+      ]);
+
+      // Real call
+      const resolver = await target.prepareTransactions();
+      await resolver.run({} as TransactionReceiptWithDecodedLogs);
+      expect(resolver.result).toEqual(shareholderObject);
+      expect(fetchStub.callCount).toBe(1);
+    });
+
+    test('should throw if there is no supplied valid security token', async () => {
+      tokenFactoryMock.set(
+        'getSecurityTokenInstanceFromTicker',
+        sinon
+          .stub()
+          .withArgs({ address: params1.symbol })
+          .throws()
+      );
+
+      expect(target.prepareTransactions()).rejects.toThrow(
+        new PolymathError({
+          code: ErrorCode.ProcedureValidationError,
+          message: `There is no Security Token with symbol ${params1.symbol}`,
+        })
+      );
+    });
+  });
+});
