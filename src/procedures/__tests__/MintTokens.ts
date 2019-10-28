f2e0b30bb4d014ddc57a864e8a86bd63214003c4
Victor Wiebe <victor.wiebe@polymath.network>
2019-10-28 09:35:20 -0800

    refactor: mint tokens to make code simpler

--- src/procedures/__tests__/MintTokens.ts
+++ src/procedures/__tests__/MintTokens.ts
@@ -10,17 +10,7 @@ import * as securityTokenEntityModule from '~/entities/SecurityToken/SecurityTok
 import { PolymathError } from '~/PolymathError';
 import { ErrorCode, MintTokensProcedureArgs, ProcedureType } from '~/types';
 import * as securityTokenFactoryModule from '~/entities/factories/SecurityTokenFactory';
-import * as cappedStoFactoryModule from '~/entities/factories/CappedStoFactory';
-import * as checkpointFactoryModule from '~/entities/factories/CheckpointFactory';
-import * as dividendDistributionSecurityTokenFactoryModule from '~/entities/factories/DividendDistributionFactory';
-import * as erc20DividendsManagerFactoryModule from '~/entities/factories/Erc20DividendsManagerFactory';
-import * as erc20TokenBalanceFactoryModule from '~/entities/factories/Erc20TokenBalanceFactory';
-import * as ethDividendsManagerFactoryModule from '~/entities/factories/EthDividendsManagerFactory';
-import * as investmentFactoryModule from '~/entities/factories/InvestmentFactory';
-import * as securityTokenReservationModule from '~/entities/factories/SecurityTokenReservationFactory';
 import * as shareholderFactoryModule from '~/entities/factories/ShareholderFactory';
-import * as usdTieredStoFactoryModule from '~/entities/factories/UsdTieredStoFactory';
-import * as taxWithholdingFactoryModule from '~/entities/factories/TaxWithholdingFactory';
 import * as utilsModule from '~/utils';
 import * as contextModule from '../../Context';
 import * as wrappersModule from '../../PolymathBase';
@@ -28,6 +18,7 @@ import * as tokenFactoryModule from '../../testUtils/MockedTokenFactoryModule';
 import * as moduleWrapperFactoryModule from '../../testUtils/MockedModuleWrapperFactoryModule';
 import { Wallet } from '~/Wallet';
 import { TransferErc20 } from '~/procedures';
+import { mockFactories } from '~/testUtils/MockFactories';
 
 const testAddress = '0x6666666666666666666666666666666666666666';
 const testAddress2 = '0x9999999999999999999999999999999999999999';
@@ -55,30 +46,14 @@ describe('MintTokens', () => {
 
   // Mock factories
   let securityTokenFactoryMock: MockManager<securityTokenFactoryModule.SecurityTokenFactory>;
-  let cappedStoFactoryMock: MockManager<cappedStoFactoryModule.CappedStoFactory>;
-  let checkpointFactoryMock: MockManager<checkpointFactoryModule.CheckpointFactory>;
-  let dividendDistributionFactoryMock: MockManager<
-    dividendDistributionSecurityTokenFactoryModule.DividendDistributionFactory
-  >;
-  let erc20DividendsManagerFactoryMock: MockManager<
-    erc20DividendsManagerFactoryModule.Erc20DividendsManagerFactory
-  >;
-  let erc20TokenBalanceFactoryMock: MockManager<
-    erc20TokenBalanceFactoryModule.Erc20TokenBalanceFactory
-  >;
-  let ethDividendsManagerFactoryMock: MockManager<
-    ethDividendsManagerFactoryModule.EthDividendsManagerFactory
-  >;
-  let investmentFactoryMock: MockManager<investmentFactoryModule.InvestmentFactory>;
-  let securityTokenReservationFactoryMock: MockManager<
-    securityTokenReservationModule.SecurityTokenReservationFactory
-  >;
   let shareholderFactoryMock: MockManager<shareholderFactoryModule.ShareholderFactory>;
-  let usdTieredStoFactoryMock: MockManager<usdTieredStoFactoryModule.UsdTieredStoFactory>;
-  let taxWithholdingFactoryMock: MockManager<taxWithholdingFactoryModule.TaxWithholdingFactory>;
   let securityTokenMock: MockManager<contractWrappersModule.SecurityToken_3_0_0>;
   let moduleFactoryMock: MockManager<contractWrappersModule.ModuleFactory_3_0_0>;
-
+  securityTokenFactoryMock = ImportMock.mockClass(
+    securityTokenFactoryModule,
+    'SecurityTokenFactory'
+  );
+  shareholderFactoryMock = ImportMock.mockClass(shareholderFactoryModule, 'ShareholderFactory');
   let shareholdersEntityMock: MockManager<shareholdersEntityModule.Shareholders>;
 
   let securityTokenEntityMock: MockManager<securityTokenEntityModule.SecurityToken>;
@@ -122,59 +97,8 @@ describe('MintTokens', () => {
       moduleFactoryMock.getMockInstance()
     );
 
-    securityTokenFactoryMock = ImportMock.mockClass(
-      securityTokenFactoryModule,
-      'SecurityTokenFactory'
-    );
+    const factoryMockSetup = mockFactories();
 
-    cappedStoFactoryMock = ImportMock.mockClass(cappedStoFactoryModule, 'CappedStoFactory');
-    checkpointFactoryMock = ImportMock.mockClass(checkpointFactoryModule, 'CheckpointFactory');
-    dividendDistributionFactoryMock = ImportMock.mockClass(
-      dividendDistributionSecurityTokenFactoryModule,
-      'DividendDistributionFactory'
-    );
-    erc20DividendsManagerFactoryMock = ImportMock.mockClass(
-      erc20DividendsManagerFactoryModule,
-      'Erc20DividendsManagerFactory'
-    );
-    erc20TokenBalanceFactoryMock = ImportMock.mockClass(
-      erc20TokenBalanceFactoryModule,
-      'Erc20TokenBalanceFactory'
-    );
-    ethDividendsManagerFactoryMock = ImportMock.mockClass(
-      ethDividendsManagerFactoryModule,
-      'EthDividendsManagerFactory'
-    );
-    investmentFactoryMock = ImportMock.mockClass(investmentFactoryModule, 'InvestmentFactory');
-    securityTokenReservationFactoryMock = ImportMock.mockClass(
-      securityTokenReservationModule,
-      'SecurityTokenReservationFactory'
-    );
-    shareholderFactoryMock = ImportMock.mockClass(shareholderFactoryModule, 'ShareholderFactory');
-    usdTieredStoFactoryMock = ImportMock.mockClass(
-      usdTieredStoFactoryModule,
-      'UsdTieredStoFactory'
-    );
-    taxWithholdingFactoryMock = ImportMock.mockClass(
-      taxWithholdingFactoryModule,
-      'TaxWithholdingFactory'
-    );
-
-    const factoryMockSetup = {
-      securityTokenFactory: securityTokenFactoryMock.getMockInstance(),
-      securityTokenReservationFactory: securityTokenReservationFactoryMock.getMockInstance(),
-      erc20TokenBalanceFactory: erc20TokenBalanceFactoryMock.getMockInstance(),
-      investmentFactory: investmentFactoryMock.getMockInstance(),
-      cappedStoFactory: cappedStoFactoryMock.getMockInstance(),
-      usdTieredStoFactory: usdTieredStoFactoryMock.getMockInstance(),
-      dividendDistributionFactory: dividendDistributionFactoryMock.getMockInstance(),
-      checkpointFactory: checkpointFactoryMock.getMockInstance(),
-      erc20DividendsManagerFactory: erc20DividendsManagerFactoryMock.getMockInstance(),
-      ethDividendsManagerFactory: ethDividendsManagerFactoryMock.getMockInstance(),
-      shareholderFactory: shareholderFactoryMock.getMockInstance(),
-      taxWithholdingFactory: taxWithholdingFactoryMock.getMockInstance(),
-    };
-    contextMock.set('factories', factoryMockSetup);
     contextMock.set('currentWallet', new Wallet({ address: () => Promise.resolve(testAddress) }));
 
     polyTokenMock = ImportMock.mockClass(contractWrappersModule, 'PolyToken');
@@ -207,11 +131,11 @@ describe('MintTokens', () => {
         isAccredited: true,
       },
     ];
-
     shareholdersEntityMock.mock('getShareholders', shareHolders);
     securityTokenEntityMock.mock('shareholders', shareholdersEntityMock.getMockInstance());
-
-    securityTokenFactoryMock.mock('fetch', securityTokenEntityMock.getMockInstance());
+    securityTokenFactoryMock.mock('fetch', securityTokenFactoryMock.getMockInstance());
+    factoryMockSetup.securityTokenFactory = securityTokenFactoryMock.getMockInstance();
+    contextMock.set('factories', factoryMockSetup);
 
     // Instantiate MintTokens
     target = new MintTokens(params1, contextMock.getMockInstance());
