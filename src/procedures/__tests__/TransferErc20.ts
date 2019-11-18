a075aaf3c031387e2aa628da153318b19740bcde
Victor Wiebe <victor.wiebe@polymath.network>
2019-11-18 09:39:06 -0800

    test: coverage increased to 100 for error cases

--- src/procedures/__tests__/TransferErc20.ts
+++ src/procedures/__tests__/TransferErc20.ts
@@ -2,7 +2,7 @@ import { ImportMock, MockManager } from 'ts-mock-imports';
 import { restore, spy } from 'sinon';
 import * as contractWrappersModule from '@polymathnetwork/contract-wrappers';
 import { BigNumber, TransactionReceiptWithDecodedLogs } from '@polymathnetwork/contract-wrappers';
-import { Procedure } from '~/procedures/Procedure';
+import { Procedure } from '../Procedure';
 import { ErrorCode, ProcedureType, TransferErc20ProcedureArgs } from '~/types';
 import * as erc20TokenBalanceFactoryModule from '~/entities/factories/Erc20TokenBalanceFactory';
 import * as contextModule from '../../Context';
@@ -11,8 +11,11 @@ import * as tokenFactoryModule from '../../testUtils/MockedTokenFactoryModule';
 import * as moduleWrapperFactoryModule from '../../testUtils/MockedModuleWrapperFactoryModule';
 import { Wallet } from '~/Wallet';
 import { TransferErc20 } from '~/procedures';
-import { mockFactories } from '~/testUtils/mockFactories';
-import { PolymathError } from '~/PolymathError';
+import * as transferErc20Module from '~/procedures/TransferErc20';
+import { mockFactories } from '../../testUtils/mockFactories';
+import { PolymathError } from '../../PolymathError';
+import { Erc20TokenBalance } from '../../entities';
+import { Factories } from '../../Context';
 
 const params: TransferErc20ProcedureArgs = {
   amount: new BigNumber(10),
@@ -38,9 +41,10 @@ describe('TransferErc20', () => {
   let securityTokenRegistryMock: MockManager<contractWrappersModule.SecurityTokenRegistry>;
 
   let erc20Mock: MockManager<contractWrappersModule.ERC20>;
+  let factoryMockSetup: Factories;
 
   beforeEach(() => {
-    // Mock the context, wrappers, and tokenFactory to test LaunchCappedSto
+    // Mock the context, wrappers, and tokenFactory to test TransferErc20
     contextMock = ImportMock.mockClass(contextModule, 'Context');
     wrappersMock = ImportMock.mockClass(wrappersModule, 'PolymathBase');
     tokenFactoryMock = ImportMock.mockClass(tokenFactoryModule, 'MockedTokenFactoryModule');
@@ -74,7 +78,7 @@ describe('TransferErc20', () => {
       'Erc20TokenBalanceFactory'
     );
 
-    const factoryMockSetup = mockFactories();
+    factoryMockSetup = mockFactories();
     factoryMockSetup.erc20TokenBalanceFactory = erc20TokenBalanceFactoryMock.getMockInstance();
 
     erc20TokenBalanceFactoryMock.mock('refresh', {});
@@ -85,7 +89,7 @@ describe('TransferErc20', () => {
     );
 
     polyTokenMock = ImportMock.mockClass(contractWrappersModule, 'PolyToken');
-    polyTokenMock.mock('address', Promise.resolve(params.receiver));
+    polyTokenMock.mock('address', Promise.resolve(params.tokenAddress));
     wrappersMock.set('polyToken', polyTokenMock.getMockInstance());
     wrappersMock.mock('isTestnet', Promise.resolve(false));
 
@@ -118,6 +122,20 @@ describe('TransferErc20', () => {
       expect(addTransactionSpy.callCount).toEqual(1);
     });
 
+    test('should throw if supplied address does not correspond to erc20 token', async () => {
+      wrappersMock
+        .mock('getERC20TokenWrapper')
+        .withArgs({ address: params.tokenAddress })
+        .throws();
+
+      await expect(target.prepareTransactions()).rejects.toThrow(
+        new PolymathError({
+          code: ErrorCode.ProcedureValidationError,
+          message: 'The supplied address does not correspond to an ERC20 token',
+        })
+      );
+    });
+
     test('should throw if address belongs to a security token, not an erc20 token', async () => {
       securityTokenRegistryMock.mock('isSecurityToken', Promise.resolve(true));
 
@@ -130,6 +148,23 @@ describe('TransferErc20', () => {
       );
     });
 
+    test('should add a transaction to the queue to transfer an erc20 token getting token funds if there are not enough funds, only on testnet', async () => {
+      wrappersMock.mock('isTestnet', Promise.resolve(true));
+      erc20Mock.mock('balanceOf', Promise.resolve(new BigNumber(2)));
+      const addTransactionSpy = spy(target, 'addTransaction');
+      // Real call
+      await target.prepareTransactions();
+
+      // Verifications
+      expect(addTransactionSpy.getCall(0).calledWith(erc20Mock.getMockInstance().transfer)).toEqual(
+        true
+      );
+      expect(
+        addTransactionSpy.getCall(1).calledWith(wrappersMock.getMockInstance().getPolyTokens)
+      ).toEqual(true);
+      expect(addTransactionSpy.callCount).toEqual(2);
+    });
+
     test('should throw if there are not enough funds to make transfer', async () => {
       erc20Mock.mock('balanceOf', Promise.resolve(new BigNumber(2)));
 
@@ -139,6 +174,23 @@ describe('TransferErc20', () => {
           message: 'Not enough funds',
         })
       );
+    });
+
+    test('should successfully create erc20 transfer resolver', async () => {
+      const refreshStub = erc20TokenBalanceFactoryMock.mock('refresh', Promise.resolve(undefined));
+      const address = params.tokenAddress ? params.tokenAddress : '';
+      const erc20TokenBalanceGeneratedId = Erc20TokenBalance.generateId({
+        tokenAddress: address,
+        walletAddress: params.receiver,
+      });
+      const resolverValue = await transferErc20Module.createTransferErc20Resolver(
+        factoryMockSetup,
+        address,
+        params.receiver
+      )();
+      expect(refreshStub.getCall(0).calledWithExactly(erc20TokenBalanceGeneratedId)).toEqual(true);
+      expect(await resolverValue).toEqual(undefined);
+      expect(refreshStub.callCount).toEqual(1);
     });
   });
 });
