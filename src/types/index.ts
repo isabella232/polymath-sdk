fbafd4c856950e01912d37ac00288557d500f4b7
Jeremias Diaz <monitz87@gmail.com>
2019-11-20 18:20:38 -0300

    feat: add support for shareholder count restrictions
    Also added the restrictions namespace and fixed revert errors not being wrapped properly

--- src/types/index.ts
+++ src/types/index.ts
@@ -104,6 +104,7 @@ export enum ProcedureType {
   EnableDividendManagers = 'EnableDividendManagers',
   EnableGeneralPermissionManager = 'EnableGeneralPermissionManager',
   EnableGeneralTransferManager = 'EnableGeneralTransferManager',
+  EnableCountTransferManager = 'EnableCountTransferManager',
   LaunchCappedSto = 'LaunchCappedSto',
   LaunchUsdTieredSto = 'LaunchUsdTieredSto',
   CreateErc20DividendDistribution = 'CreateErc20DividendDistribution',
@@ -123,6 +124,7 @@ export enum ProcedureType {
   ModifyShareholderData = 'ModifyShareholderData',
   RevokeKyc = 'RevokeKyc',
   MintTokens = 'MintTokens',
+  ModifyMaxHolderCount = 'ModifyMaxHolderCount',
 }
 
 export enum PolyTransactionTag {
@@ -142,6 +144,7 @@ export enum PolyTransactionTag {
   EnableUsdTieredSto = 'EnableUsdTieredSto',
   EnableGeneralPermissionManager = 'EnableGeneralPermissionManager',
   EnableGeneralTransferManager = 'EnableGeneralTransferManager',
+  EnableCountTransferManager = 'EnableCountTransferManager',
   DisableFeature = 'DisableFeature',
   ReclaimDividendFunds = 'ReclaimDividendFunds',
   WithdrawTaxWithholdings = 'WithdrawTaxWithholdings',
@@ -155,6 +158,7 @@ export enum PolyTransactionTag {
   ModifyKycDataMulti = 'ModifyKycDataMulti',
   ModifyInvestorFlagMulti = 'ModifyInvestorFlagMulti',
   IssueMulti = 'IssueMulti',
+  ChangeHolderCount = 'ChangeHolderCount',
 }
 
 export type MaybeResolver<T> = PostTransactionResolver<T> | T;
@@ -248,6 +252,11 @@ export interface EnableGeneralPermissionManagerProcedureArgs {
 
 export interface EnableGeneralTransferManagerProcedureArgs {
   symbol: string;
+}
+
+export interface EnableCountTransferManagerProcedureArgs {
+  symbol: string;
+  maxHolderCount: number;
 }
 
 export interface DisableFeatureProcedureArgs {
@@ -408,6 +417,11 @@ export interface RevokeKycProcedureArgs {
   shareholderAddresses: string[];
 }
 
+export interface ModifyMaxHolderCountProcedureArgs {
+  symbol: string;
+  maxHolderCount: number;
+}
+
 export interface ProcedureArguments {
   [ProcedureType.ApproveErc20]: ApproveErc20ProcedureArgs;
   [ProcedureType.TransferErc20]: TransferErc20ProcedureArgs;
@@ -416,6 +430,9 @@ export interface ProcedureArguments {
   [ProcedureType.CreateEtherDividendDistribution]: CreateEtherDividendDistributionProcedureArgs;
   [ProcedureType.CreateSecurityToken]: CreateSecurityTokenProcedureArgs;
   [ProcedureType.EnableDividendManagers]: EnableDividendManagersProcedureArgs;
+  [ProcedureType.EnableGeneralPermissionManager]: EnableGeneralPermissionManagerProcedureArgs;
+  [ProcedureType.EnableGeneralTransferManager]: EnableGeneralTransferManagerProcedureArgs;
+  [ProcedureType.EnableCountTransferManager]: EnableCountTransferManagerProcedureArgs;
   [ProcedureType.ReclaimFunds]: ReclaimFundsProcedureArgs;
   [ProcedureType.ReserveSecurityToken]: ReserveSecurityTokenProcedureArgs;
   [ProcedureType.WithdrawTaxes]: WithdrawTaxesProcedureArgs;
@@ -496,6 +513,7 @@ export enum Feature {
   Shareholders = 'Shareholders',
   Erc20Dividends = 'Erc20Dividends',
   EtherDividends = 'EtherDividends',
+  ShareholderCountRestrictions = 'ShareholderCountRestrictions',
 }
 
 export enum SecurityTokenRole {
@@ -505,6 +523,7 @@ export enum SecurityTokenRole {
   EtherDividendsOperator = 'EtherDividendsOperator',
   EtherDividendsAdministrator = 'EtherDividendsAdministrator',
   ShareholdersAdministrator = 'ShareholdersAdministrator',
+  ShareholderCountRestrictionsAdministrator = 'ShareholderCountRestrictionsAdministrator',
 }
 
 export enum StoRole {
