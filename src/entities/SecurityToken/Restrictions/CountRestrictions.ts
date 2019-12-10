d0ef9e933a214f282616cfbf9f4200e965841838
Jeremias Diaz <monitz87@gmail.com>
2019-12-10 16:59:42 -0300

    feat: separate transfer restrictions into namespaces
    Create a general `Restrictions` namespace
    
    BREAKING CHANGE: subdivide `restrictions` namespace into `count` and `percentage` and distributed
    the methods between them

---
+++ src/entities/SecurityToken/Restrictions/CountRestrictions.ts
@@ -0,0 +1,50 @@
+import { ModuleName } from '@polymathnetwork/contract-wrappers';
+import { ErrorCode } from '../../../types';
+import { ModifyMaxHolderCount } from '../../../procedures';
+import { SubModule } from '../SubModule';
+import { PolymathError } from '../../../PolymathError';
+
+export class CountRestrictions extends SubModule {
+  /**
+   * Modify the maximum amount of shareholders allowed to hold the token at once
+   *
+   * @param maxHolderCount limit to the amount of concurrent shareholders
+   */
+  public modifyMaxHolderCount = async (args: { maxHolderCount: number }) => {
+    const procedure = new ModifyMaxHolderCount(
+      {
+        symbol: this.securityToken.symbol,
+        ...args,
+      },
+      this.context
+    );
+    return procedure.prepare();
+  };
+
+  /**
+   * Retrieve the maximum amonut of shareholders allowed to hold the token at once.
+   * Can be modified with `modifyMaxHolderCount`
+   */
+  public getMaxHolderCount = async () => {
+    const {
+      context: { contractWrappers },
+      securityToken,
+    } = this;
+
+    const { symbol } = securityToken;
+
+    const countTransferManagerModule = (await contractWrappers.getAttachedModules(
+      { moduleName: ModuleName.CountTransferManager, symbol },
+      { unarchived: true }
+    ))[0];
+
+    if (!countTransferManagerModule) {
+      throw new PolymathError({
+        code: ErrorCode.FeatureNotEnabled,
+        message: 'You must enable the ShareholderCountRestrictions Feature',
+      });
+    }
+
+    return countTransferManagerModule.maxHolderCount();
+  };
+}
