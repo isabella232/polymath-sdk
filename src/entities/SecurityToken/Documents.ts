9a801b0e3fb3188866e2f4af9cf5dd16a7658887
Victor Wiebe <victor.wiebe@polymath.network>
2019-12-12 15:18:55 -0800

    feat: add getters for documents in submodule

--- src/entities/SecurityToken/Documents.ts
+++ src/entities/SecurityToken/Documents.ts
@@ -1,3 +1,61 @@
 import { SubModule } from './SubModule';
+import { PolymathError } from '../../PolymathError';
+import { ErrorCode } from '../../types';
 
-export class Documents extends SubModule {}
+interface DocumentData {
+  documentUri: string;
+  documentHash: string;
+  documentTime: Date;
+}
+
+export class Documents extends SubModule {
+  /**
+   * Retrieve a specific document's data by name
+   */
+  public getDocument = async (documentName: { name: string }): Promise<DocumentData> => {
+    const {
+      context: { contractWrappers },
+      securityToken,
+    } = this;
+
+    const { symbol } = securityToken;
+    let securityTokenInstance;
+
+    try {
+      securityTokenInstance = await contractWrappers.tokenFactory.getSecurityTokenInstanceFromTicker(
+        symbol
+      );
+    } catch (err) {
+      throw new PolymathError({
+        code: ErrorCode.ProcedureValidationError,
+        message: `There is no Security Token with symbol ${symbol}`,
+      });
+    }
+    return securityTokenInstance.getDocument(documentName);
+  };
+
+  /**
+   * Retrieve a list of all document names
+   */
+  public getAllDocuments = async (): Promise<string[]> => {
+    const {
+      context: { contractWrappers },
+      securityToken,
+    } = this;
+
+    const { symbol } = securityToken;
+    let securityTokenInstance;
+
+    try {
+      securityTokenInstance = await contractWrappers.tokenFactory.getSecurityTokenInstanceFromTicker(
+        symbol
+      );
+    } catch (err) {
+      throw new PolymathError({
+        code: ErrorCode.ProcedureValidationError,
+        message: `There is no Security Token with symbol ${symbol}`,
+      });
+    }
+    return securityTokenInstance.getAllDocuments();
+  };
+}
