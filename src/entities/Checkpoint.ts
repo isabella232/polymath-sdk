f92ca5eb036e710d09d2014094188d4f7b738c63
Shuffledex <shuffledex@gmail.com>
2020-01-03 15:07:47 -0300

    docs: add checkpoint entity documentation

--- src/entities/Checkpoint.ts
+++ src/entities/Checkpoint.ts
@@ -5,26 +5,64 @@ import { DividendDistribution } from './DividendDistribution';
 import { ShareholderBalance, ErrorCode } from '../types';
 import { PolymathError } from '../PolymathError';
 
+/**
+ * Represents a checkpoint identifier
+ */
 export interface UniqueIdentifiers {
+  /**
+   * st identifier
+   */
   securityTokenId: string;
+  /**
+   * checkpoint id used to query historical balances
+   */
   index: number;
 }
 
+/**
+ * Check if the provided value is of type [[UniqueIdentifiers]]
+ *
+ * @param identifiers - internal checkpoint representation
+ */
 function isUniqueIdentifiers(identifiers: any): identifiers is UniqueIdentifiers {
   const { securityTokenSymbol, index } = identifiers;
 
   return typeof securityTokenSymbol === 'string' && typeof index === 'number';
 }
 
+/**
+ * Represents a single checkpoint instance
+ */
 export interface Params {
+  /**
+   * information for distributing dividends to holders
+   */
   dividendDistributions: DividendDistribution[];
+  /**
+   * symbol of the security token
+   */
   securityTokenSymbol: string;
+  /**
+   * balances for security token holders
+   */
   shareholderBalances: ShareholderBalance[];
+  /**
+   * total number of st market cap
+   */
   totalSupply: BigNumber;
+  /**
+   * date of checkpoint creation
+   */
   createdAt: Date;
 }
 
+/**
+ * Checkpoint implementation used to manage Security Token checkpoint functionality
+ */
 export class Checkpoint extends Entity<Params> {
+  /**
+   * Transform checkpoint object to plain string
+   */
   public static generateId({ securityTokenId, index }: UniqueIdentifiers) {
     return serialize('checkpoint', {
       securityTokenId,
@@ -32,6 +70,11 @@ export class Checkpoint extends Entity<Params> {
     });
   }
 
+  /**
+   * Unserialize string to Checkpoint object
+   *
+   * @param serialize - checkpoint serialized representation
+   */
   public static unserialize(serialized: string) {
     const unserialized = unserialize(serialized);
 
@@ -61,6 +104,9 @@ export class Checkpoint extends Entity<Params> {
 
   public createdAt: Date;
 
+  /**
+   * Create a new Chekpoint instance
+   */
   constructor(params: Params & UniqueIdentifiers) {
     super();
 
@@ -84,6 +130,9 @@ export class Checkpoint extends Entity<Params> {
     this.uid = Checkpoint.generateId({ securityTokenId, index });
   }
 
+  /**
+   * Convert entity as a POJO (Plain Old Javascript Object)
+   */
   public toPojo() {
     const {
       uid,
@@ -108,6 +157,9 @@ export class Checkpoint extends Entity<Params> {
     };
   }
 
+  /**
+   * Hydrating the entity
+   */
   public _refresh(params: Partial<Params>) {
     const {
       dividendDistributions,
